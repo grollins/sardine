aa71e3ac1dc8fe3c1ee3cbcd617fac4ba101cb38
Geoff Rollins <grollins@gmail.com>
2013-09-23 10:45:17 -0700

    Modify examples to minimize using gradients.

--- examples/triatomic/compute_modes.py
+++ examples/triatomic/compute_modes.py
@@ -5,11 +5,12 @@ Computes normal modes for a triatomic molecule.
 from os import mkdir
 from os.path import exists, join
 from sardine.universe import UniverseFactory
-from sardine.energy import BondEnergyFactory, AngleEnergyFactory, VDWEnergyFactory,\
-                           EnergyFunctionFactory
+from sardine.energy import BondEnergyFactory, AngleEnergyFactory, VDWEnergyFactory
+from sardine.energy import EnergyFunctionFactory, GradientFunctionFactory
 from sardine.nma import compute_hessian, compute_force_constant_matrix,\
                         compute_normal_modes, generate_mode_trajectory
 from sardine.trajectory import save_trajectory_to_pdb
+from sardine.minimize import BFGSMinimizer
 
 
 PDB_FILENAME = "triatomic.pdb"
@@ -25,19 +26,44 @@ def main():
     uf.load_atoms_from_file(PDB_FILENAME)
     universe = uf.create_universe()
 
-    bond_energy = BondEnergyFactory()
-    bond_energy.load_bonds_from_file(SF_FILENAME)
-    bond_energy_func = bond_energy.create_energy_func(num_atoms=len(universe))
+    bond_energy_factory = BondEnergyFactory()
+    bond_energy_factory.load_bonds_from_file(SF_FILENAME)
+    bond_energy_func = bond_energy_factory.create_energy_func(num_atoms=len(universe))
+    bond_gradient_func = bond_energy_factory.create_gradient_func(num_atoms=len(universe))
 
     eff = EnergyFunctionFactory()
     eff.add_energy_term('bonds', bond_energy_func)
     energy_func = eff.create_energy_func(['bonds',], num_atoms=len(universe))
 
+    gff = GradientFunctionFactory()
+    gff.add_gradient_term('bonds', bond_gradient_func)
+    gradient_func = gff.create_gradient_func(['bonds',], num_atoms=len(universe))
+
+    # ======================
+    # = Minimize structure =
+    # ======================
+    minimizer = BFGSMinimizer(maxiter=200)
+    X = universe.get_coords().flatten()
+    energy_initial = energy_func(X)
+    X_min, energy_min = minimizer.run_minimization(
+                            energy_func, gradient_func, X,
+                            num_atoms=len(universe),
+                            save_trajectory=True, noisy=True)
+    print energy_initial, energy_min
+    trajectory = minimizer.get_trajectory()
+    save_trajectory_to_pdb('minimization.pdb', trajectory, universe,
+                           bond_energy_factory)
+    print "Wrote minimization.pdb"
+
+    # ========================
+    # = Compute normal modes =
+    # ========================
     M = universe.get_inv_sqrt_mass_matrix()
-    X = universe.get_coords()
+    # X = universe.get_coords()
+    X = X_min # use minimized coordinates
     H = compute_hessian(energy_func, X)
     F = compute_force_constant_matrix(H, M)
-    normal_modes = compute_normal_modes(F, discard_trans_and_rot=False)
+    normal_modes = compute_normal_modes(F, discard_trans_and_rot=True)
     mode_freqs = normal_modes.get_frequencies()
 
     with open(join(OUTPUT_DIR, 'eigen_values.txt'), 'w') as f:
@@ -48,7 +74,8 @@ def main():
                                                    mode_number=i)
         save_trajectory_to_pdb(
             join(OUTPUT_DIR, 'triatomic_mode%02d.pdb') % (i+1),
-            mode_trajectory, universe, bond_energy)
+            mode_trajectory, universe, bond_energy_factory)
+        print "Wrote triatomic_mode%02d.pdb" % (i+1)
 
 if __name__ == '__main__':
     main()
